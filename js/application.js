5ab0e4d1498ed7f148ef1c69200187021bf30f2e
mfeiden <61667160+mfeiden@users.noreply.github.com>
2020-05-21 17:13:55 -0500

    final adjustments...

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
-var updateCost = (function() {
+// calculate cost per item & total
+var updateCost = function() {
   var costArr = [];
   $('tbody tr').each(function (i, item) {
     var linePrice = parseFloat($(item).find('.price').text());
@@ -7,15 +8,47 @@ var updateCost = (function() {
     if(quantity) {
       $(item).children('.cost').html(itemCost.toFixed(2));
       costArr.push(itemCost);
-    };
+    } else {
+      $(item).children('.cost').html('');
+    }
   });
   var total = costArr.length > 0 ? costArr.reduce((sum, num) => sum + num) : 0;
-    $('#grandTotal').html(total.toFixed(2)); 
-});
+  $('#grandTotal').html(total.toFixed(2)); 
+};
 
-$(document).ready(updateCost);
+// add item to cart
+var addItem = function() {
+  var newItem = $('#item').val();
+  var newPrice = parseFloat($('#price').val()).toFixed(2);
+  if (!newItem || isNaN(newPrice)) {
+    alert('You must enter both item name and unit price to add a new item.');
+  } else {
+    $('#lastRow').before("<tr><td class='item'>" + newItem + "</td><td class='price'>" + newPrice + "</td><td class='qty'><input type='number'></input></td><td class='cost'></td><td><button class='btn btn-xs remove'><i>Remove</i></button></td></tr>");  
+  }
+  $('tr').find('#item, #price').val('');
+};
 
-$(document).on('click', '.remove', function() {
+// remove item from cart
+var removeItem = function() {
   $(this).closest('tr').remove();
   updateCost();
+};
+
+// update cart when quantities change
+var updateQty = function () {
+  clearTimeout(delay);
+  var delay = setTimeout(updateCost, 1000);
+};
+
+// event handlers
+$(document).ready(function() {
+  updateCost();
+  $(document).on('input', '.qty', updateQty);
+  $(document).on('click', '.remove', removeItem);
+  $(document).on('click', '.add', addItem);
+  $('#price').on('keyup', function(event) {
+    if (event.key === 'Enter') {
+      addItem();
+    }
+  });
 });
